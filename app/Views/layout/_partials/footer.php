62f0d14b185cb2635a369d02ebd97144007d191b
Nabiel Omar Syarif <nabilumarsyarif294@gmail.com>
2021-05-08 20:55:01 +0700

    modify copyright

--- app/Views/layout/_partials/footer.php
+++ app/Views/layout/_partials/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <script> document.write(new Date().getFullYear()); </script> - <b>Sriwijaya University</b> - All Rights Reserved
+            <span>Copyright &copy; <script> document.write(new Date().getFullYear()); </script> - <b>Sriwijaya University</b>. All Rights Reserved
             </span>
         </div>
     </div>
